acd892b0643d5c4aea7a1da568c7d89a6593875e
Corentin THEVENET <cthevenet@talentsoft.com>
2022-10-20 21:30:40 +0200

    initial commit

---
+++ Lucca_Suite/ExchangeRate.cs
@@ -0,0 +1,16 @@
+﻿namespace Lucca_Suite
+{
+    public class ExchangeRate
+    {
+        public ExchangeRate(string sourceCurrencySymbol, double rate, string destinationCurrencySymbol)
+        {
+            SourceCurrencySymbol = sourceCurrencySymbol;
+            Rate = rate;
+            DestinationCurrencySymbol = destinationCurrencySymbol;  
+        }
+
+        public string SourceCurrencySymbol { get; private set; }
+        public double Rate { get; private set; }
+        public string DestinationCurrencySymbol { get; private set; }
+    }
+}
---
+++ Lucca_Suite/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Enter file path : ");
+        //string pathRead = Console.ReadLine();
+
+        // C:\Users\Corentin\source\repos\Lucca_Suite\Lucca_Suite\input.txt
+        //string text = File.ReadAllText(pathRead);
+        string text = File.ReadAllText("C:\\Users\\Corentin\\source\\repos\\Lucca_Suite\\Lucca_Suite\\input.txt");
+
+        //var currencies = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+        //    .Select(ci => ci.LCID).Distinct()
+        //    .Select(id => new RegionInfo(id))
+        //    .Select(r => r.ISOCurrencySymbol).Distinct();
+
+        //foreach (var currency in currencies)
+        //{
+        //    Console.WriteLine($"currency : {currency}");
+        //}
+        //// 107 currencies
+        //Console.WriteLine($"number of currency : {currencies.Count()}");
+
+
+        //Console.WriteLine(text);
+
+        //Console.WriteLine("end execution");
+    }
+}
